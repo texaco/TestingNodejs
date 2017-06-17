9a46f2504c1c84b4cc0fb1386926589acfdb67d7
manolo texaco <manolo.texaco@gmail.com>
2017-06-17 20:56:53 +0200

    Testing different javascript stuff

---
+++ testing.js
@@ -0,0 +1,9 @@
+// Testing stuff about javascript basic sintax.
+var string1 = 'String to show';
+var string2 = "String to show";
+console.log(string1);
+console.log(string2);
+
+var string3 = 'String to show'.toUpperCase();
+console.log(string3);
+
