50ef4bec6bc4bbe94179d9e5fa0514fe6d8e940b
manolo texaco <manolo.texaco@gmail.com>
2017-06-17 20:59:34 +0200

    Testing numbrers

--- testing.js
+++ testing.js
@@ -1,4 +1,7 @@
 // Testing stuff about javascript basic sintax.
+
+// Strings
+
 var string1 = 'String to show';
 var string2 = "String to show";
 console.log(string1);
@@ -7,3 +10,14 @@ console.log(string2);
 var string3 = 'String to show'.toUpperCase();
 console.log(string3);
 
+// Numbers
+
+var num1 = 123456;
+var num2 = 123 + 456;
+console.log(num1);
+console.log(num2);
+
+console.log(num1 + num2);
+console.log(num1 / num2);
+
+// Booleans
