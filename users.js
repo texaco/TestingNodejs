83210fc80f293c67e08775d3919eb10c922da9d2
manolo texaco <manolo.texaco@gmail.com>
2017-06-19 22:10:26 +0200

    rest api: addUser - hardcoded.

--- users.js
+++ users.js
@@ -3,10 +3,30 @@ const fs = require("fs");
 const app = express();
 const port = 3000;
 
+const user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
 app.get('/listUsers', (req, res) => {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data){
        console.log( data );
        res.end( data );
+   });
+});
+
+
+app.post('/addUser', (req, res) => {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', (err, data) => {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
    });
 });
 
