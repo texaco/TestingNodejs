a3cc59b05ad75bfdd9e4e0cf3dcc1afd946ee10f
manolo texaco <manolo.texaco@gmail.com>
2017-06-19 22:04:02 +0200

    rest api: List of users

---
+++ users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get('/listUsers', (req, res) => {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data){
+       console.log( data );
+       res.end( data );
+   });
+});
+
+const server = app.listen(port, () => {
+  console.log("Example app listening at http://%s:%s", server.address().address, server.address().port);
+});
